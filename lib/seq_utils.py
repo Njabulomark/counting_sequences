879de83ccd9dfee88148aa6465382eccaf898d0e
Njabulo Dlamini <njabulomarkdlamini@gmail.com>
2015-03-12 15:22:37 +0200

    counting_sequences/

--- lib/seq_utils.py
+++ lib/seq_utils.py
@@ -1,1 +1,17 @@
-# An empty file
+#!/usr/bin/env python
+
+import sys
+
+def count_seqs(filename):
+        line_cnts =0
+	for line in filename:
+		line = line.lstrip()
+		if line.startswith('>'):
+			line_cnts +=1
+	return line_cnts
+
+
+
+
+
+
